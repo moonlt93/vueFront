dec6fc2e92b7018021154b1018d8fe7a6cf032ee
moonl <moonlt93@naver.com>
2023-03-10 18:36:00 +0900

    init commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/common.css'
+import axios from 'axios'
+
+
+const app = createApp(App)
+app.config.globalProperties.$axios =axios;
+app.config.globalProperties.$serverUrl='//localhost:8081'
+app.use(router).mount('#app')
+
---
+++ src/views/PageHome.vue
@@ -0,0 +1,20 @@
+<!-- PageHome.vue -->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HellowYourWorld msg="Welcome to Your Vue.js App jinsu"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HellowYourWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'PageHome', // 여기 작성한 내용을 보여주는 것 . 변수명 바꿔주면 그대로 가져오네
+  components: {
+    HellowYourWorld
+  }
+}
+</script>
